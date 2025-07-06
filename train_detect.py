04475012ad0b6944a39ea2fff6443d2f55686be5
Frank Xu <callmedwade3@gmail.com>
2025-07-06 09:53:23 +0800

    Add local project files

---
+++ train_detect.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+
+model = YOLO("yolo_weights/yolov8m.pt")
+# Train the model on the own dataset for 50 epochs
+train_results = model.train(
+    data="train.yaml",  # Path to dataset configuration file
+    epochs=100,  # Number of training epochs
+    imgsz=640,  # Image size for training
+    device=[0,1],  # Device to run on (e.g., 'cpu', 0, [0,1,2,3])
+    batch=32  # Batch size
+)
